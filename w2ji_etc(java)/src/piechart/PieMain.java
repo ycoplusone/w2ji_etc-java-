0dd5738d3bef5d2e4afc44d2ef21d3e71bcf08e3
DLIVE <DLIVE@DESKTOP-6OUHHUK>
2020-07-23 08:56:53 +0900

    20200723- edit

--- w2ji_etc(java)/src/piechart/PieMain.java
+++ w2ji_etc(java)/src/piechart/PieMain.java
@@ -20,49 +20,29 @@ public class PieMain {
 	public static void main(String[] argv) {
 		System.out.println("----- PieMain Start -----");
 		System.out.println("read file");
-		List<DataModel> dm  = load_file();
-		/*
-		for (DataModel dataModel : dm) {
+		loadFile lf = new loadFile();
+		
+		;
+		
+		
+		for (DataModel dataModel : lf.dm) {
 			System.out.println( "dataModel : "+dataModel.getName()+" , "+dataModel.getColor() )  ;
 		}
-		*/		
-		PieChart pc = new PieChart(dm);
+		
+		
+		PieChart pc = new PieChart( lf.dm );
 			
 		
 		JFrame fr = new JFrame();
-		fr.setTitle("Pie Chart");
+		fr.setTitle( lf.title );
 		fr.getContentPane().add( pc );
 		fr.setSize(700, 500);
 		fr.setVisible(true);
 		
+		
 	}
 	
-	public static List<DataModel> load_file() {
-		List<DataModel> dm = new ArrayList<DataModel>();
-		String path = PieMain.class.getResource("").getPath().substring(1); 
-		BufferedReader br = null;
-	    try{
-	        File file = new File(path +"animal_data.txt");
-	    	 br = Files.newBufferedReader(Paths.get( path +"animal_data.txt" ));
-	    	 String line = "";
-	           while((line = br.readLine()) != null){
-	        	   DataModel ttt = new DataModel();
-	               String array[] = line.split(",");
-	               ttt.setName( array[0] );
-	               ttt.setValue( Integer.parseInt( array[1] ) );
-	               int r = (int) (Math.random() * 255 + 1);
-	               int g = (int) (Math.random() * 255 + 1);
-	               int b = (int) (Math.random() * 255 + 1);
-	               Color rgb = new Color(r,g,b);
-	               ttt.setColor(rgb);	               	               
-	               dm.add(ttt);
-	           }
-	    }catch (FileNotFoundException e) {	
-	    }catch(IOException e){
-	        System.out.println(e);
-	    }
-	    return dm;	    
-	}	
+
 	
 }
 
---
+++ w2ji_etc(java)/src/piechart/loadFile.java
@@ -0,0 +1,53 @@
+package piechart;
+
+import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class loadFile {
+	public List<DataModel> dm = new ArrayList<DataModel>();
+	public String title = "";
+	loadFile(){
+		loadingfile();
+	}
+	
+	public void loadingfile() {
+		System.out.println("--- loadFile : start ---");
+		
+		String path = loadFile.class.getResource("").getPath().substring(1); 
+		BufferedReader br = null;
+	    try{
+	        //File file = new File(path +"animal_data.txt");
+	    	 br = Files.newBufferedReader(Paths.get( path +"AnimalData.txt" ));
+	    	 String line = "";
+	           while((line = br.readLine()) != null){
+	        	   System.out.println("line  : "+line);
+	        	   if(title.equals("")) {
+	        		   title = line;
+	        	   }else {
+	        		   DataModel ttt = new DataModel();
+		               String array[] = line.split(",");
+		               ttt.setName( array[0] );
+		               ttt.setValue( Integer.parseInt( array[1] ) );
+		               int r = (int) (Math.random() * 255 + 1);
+		               int g = (int) (Math.random() * 255 + 1);
+		               int b = (int) (Math.random() * 255 + 1);
+		               Color rgb = new Color(r,g,b);
+		               ttt.setColor(rgb);	               	               
+		               dm.add(ttt);	        		   
+	        	   }
+	        	   
+	           }
+	    }catch (FileNotFoundException e) {	
+	    }catch(IOException e){
+	        System.out.println(e);
+	    }
+    
+	}	
+
+}
